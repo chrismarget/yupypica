6cc457af79bf1d5e7f78a89c3fc865cace763609
cmarget <cmarget@mutualink.net>
2020-08-14 21:14:00 -0400

    update clock, import variables

--- lib/yupypica/display.py
+++ lib/yupypica/display.py
@@ -1,36 +1,45 @@
+import datetime
+import pytz
 import urwid
 
+
+def now():
+    return datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d-%H:%M:%S%z')
 
 class Display (object):
     def __init__(self):
 
-        self.page_title = urwid.Text("initial title", align='left')
-        self.top_left = urwid.Padding(self.page_title, left=1)
+        self.title = urwid.Text("initial title", align='left')
+        top_left = urwid.Padding(self.title, left=1)
 
-        self.app_name = urwid.Text('pipyca', align='right')
-        self.top_right = urwid.Padding(self.app_name, right=1)
+        top_right = urwid.Padding(urwid.Text('yupypica', align='right'), right=1)
 
-        self.header = urwid.Columns([self.top_left, self.top_right])
+        header = urwid.Columns([top_left, top_right])
 
         self.main_box = urwid.Filler(urwid.Text("text in MainBox", align='center'))
 
         self.ver_text = urwid.Text("v0.0", align='left')
-        self.bottom_left = urwid.Padding(self.ver_text, left=1)
+        bottom_left = urwid.Padding(self.ver_text, left=1)
 
-        self.clock = urwid.Text("clock goes here", align='right')
-        self.bottom_right = urwid.Padding(self.clock, right=1)
+        self.clock = urwid.Text(now(), align='right')
+        bottom_right = urwid.Padding(self.clock, right=1)
 
-        self.footer = urwid.Columns([self.bottom_left, self.bottom_right])
+        footer = urwid.Columns([bottom_left, bottom_right])
 
-        self.frame = urwid.Frame(self.main_box, self.header, self.footer)
+        self.frame = urwid.Frame(self.main_box, header, footer)
+
+        self.loop = urwid.MainLoop(self.frame, unhandled_input=self.exit_on_q())
 
     def set_title(self, title):
-        self.page_title.set_text(title)
+        self.title.set_text(title)
 
+    def update_clock(self, loop, data):
+        self.clock.set_text(now())
+        loop.set_alarm_in(1, self.update_clock)
 
     def start(self):
-        loop = urwid.MainLoop(self.frame, unhandled_input=self.exit_on_q())
-        loop.run()
+        self.loop.set_alarm_in(1, self.update_clock)
+        self.loop.run()
 
     def exit_on_q(key):
         if key in ('q', 'Q'):
