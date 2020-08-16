812d03fb5f77459c666b39eb162a2c7501cd87c1
cmarget <cmarget@mutualink.net>
2020-08-16 15:36:58 -0400

    Button has a gpizero.Button

--- lib/yupypica/button.py
+++ lib/yupypica/button.py
@@ -1,7 +1,9 @@
+from gpiozero import Button as gzbutton
+
 class Button(object):
     def __init__(self, pin, color):
-        self.pin = pin
         self.color = color
+        self.button = gzbutton(pin)
         if self._is_pressed():
             RuntimeError("%s button appears to be stuck" % color)
 
