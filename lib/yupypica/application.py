26a20049fd9efe7967c719b3ec7b838624a561ed
cmarget <cmarget@mutualink.net>
2020-08-18 13:26:49 -0400

    improved config checking

--- lib/yupypica/application.py
+++ lib/yupypica/application.py
@@ -3,27 +3,23 @@ from .display import Display
 import asyncio
 import saturnv
 
+button_count = 4
+
 
 class Application(object):
     default_conf = {
         "button_colors": ["#070", "#44f", "#770", "#700"],
         "button_pins": [17, 22, 23, 27],
-        "tz": "utc"
+        "tz": "utc",
     }
 
     def __init__(self):
         conf = saturnv.AppConf(defaults=Application.default_conf)
-        button_count = 4
 
-        # Sanity check button color count
-        bcc = len(conf["button_colors"])
-        if bcc != button_count:
-            raise (RuntimeError("configured with %d button_colors, expect %d" % (bcc, button_count)))
-
-        # Sanity check button pin count
-        bpc = len(conf["button_pins"])
-        if bpc != button_count:
-            raise (RuntimeError("configured with %d button_pins, expect %d" % (bpc, button_count)))
+        try:
+            check_config(button_count, conf)
+        except Exception as e:
+            raise (e)
 
         loop = asyncio.get_event_loop()
         self.display = Display(loop)
@@ -35,7 +31,34 @@ class Application(object):
             self.display.add_button(b)
 
     def __buttonactivecallback(self, b):
-        self.display.populate_frame()
+        self.display.button_event(data=b)
 
     def run(self):
         self.display.start()
+
+
+def check_config(count, conf):
+    for element_name in ["button_colors", "button_pins"]:
+        try:
+            check_config_element(element_name, count, conf[element_name])
+        except Exception as e:
+            raise (e)
+
+
+def check_config_element(name, count, element):
+    # check overall length
+    counted = len(element)
+    if counted != count:
+        raise (RuntimeError("%s has %d elements, expected %d" % (name, counted, count)))
+
+    # count unique items
+    d = {}
+    for i in element:
+        d[i] = None
+    counted = len(d)
+    if counted != count:
+        raise (
+            RuntimeError(
+                "%s has %d unique elements, expected %d" % (name, counted, count)
+            )
+        )
