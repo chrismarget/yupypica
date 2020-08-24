279aaa788ba96a7ff47de63a191fc72181a9ff9c
cmarget <cmarget@mutualink.net>
2020-08-24 14:10:37 -0400

    blacken version.py

--- lib/yupypica/version.py
+++ lib/yupypica/version.py
@@ -4,12 +4,12 @@ from subprocess import check_output, CalledProcessError
 
 
 def get_version():
-    d = dirname(__file__)+"/../.."
+    d = dirname(__file__) + "/../.."
     version = "v0.0.0-unknown"
 
-    if isdir(join(d, '.git')):
+    if isdir(join(d, ".git")):
         # Get the version using "git describe".
-        cmd = 'git describe --tags --match v[0-9].*'.split()
+        cmd = "git describe --tags --match v[0-9].*".split()
         try:
             version = check_output(cmd, cwd=d).decode().strip()
         except CalledProcessError:
@@ -18,13 +18,13 @@ def get_version():
         # todo: check for comitted changes since last tag
 
         # Check for uncommitted changes
-        cmd = 'git diff-index --name-only HEAD'.split()
+        cmd = "git diff-index --name-only HEAD".split()
         try:
             working = check_output(cmd).decode().strip()
         except CalledProcessError:
             return version
 
-        if working != '':
-            version += '-working'
+        if working != "":
+            version += "-working"
 
     return version
