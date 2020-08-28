a02fba1a8e24336ff9a2ce4f00c245c8f4932405
cmarget <cmarget@mutualink.net>
2020-08-28 15:37:15 -0400

    startup animation

--- lib/keyhole/screen/animate.py
+++ lib/keyhole/screen/animate.py
@@ -1,4 +1,4 @@
-def string_on_string_at_pos_with_transparency(under: str, over: str, transparent="", idx=0) -> str:
+def string_on_string_at_idx_with_transparency(under: str, over: str, transparent="", idx=0) -> str:
     """Overlays one string onto another at a given position.
     Helper function to overlay().
 
@@ -51,7 +51,96 @@ def get_overlay_substring(under: str, over: str, idx=0) -> str:
     return over
 
 
-def overlay(under: str, over: str, transparent="", pos=0) -> str:
-    pos2 = max(0, pos-len(over) + 1)
-    over = get_overlay_substring(under, over, pos)
-    return string_on_string_at_pos_with_transparency(under, over, transparent, pos2)
+def overlay(under: str, over: str, transparent="", idx=0) -> str:
+    """Overlays one string onto another, accounting for overhang at either end.
+    With idx = 10 -> "......A"
+
+    Parameters
+    ----------
+    under: str
+        The underlay string
+    over: str
+        The overlay string
+    transparent: str, optional
+        A set of transparent characters in the overlay string. These characters
+        in the overlay do not print, rather the underlay string is visible
+        "through" these characters wherever they appear. Defaults to empty set.
+    idx: int, optional
+        the index of the position in the combined strings length where the
+        overlay string begins. Defaults to 0. Sensible values are:
+            range(len(under) + len(over) - 1)
+
+    Example
+    -------
+    over = "ABCDE"
+    under = "......."
+    transparent = "C"
+
+    With idx = 0  -> "E......"
+    With idx = 1  -> "DE....."
+    With idx = 2  -> ".DE...."
+    With idx = 3  -> "B.DE..."
+    With idx = 4  -> "AB.DE.."
+    With idx = 5  -> ".AB.DE."
+    With idx = 6  -> "..AB.DE"
+    With idx = 7  -> "...AB.D"
+    With idx = 8  -> "....AB."
+    With idx = 9  -> ".....AB"
+    With idx = 10 -> "......A"
+    """
+
+    idx2 = max(0, idx-len(over) + 1)
+    over = get_overlay_substring(under, over, idx)
+    return string_on_string_at_idx_with_transparency(under, over, transparent, idx2)
+
+def overlay_multiline(under: [], over:[], transparent="", idx=0, skiplines=0) -> []:
+    """multiline version of overlay()
+
+    Parameters
+    ----------
+    under: []
+        A list of underlay strings
+    over: []
+        A list of overlay strings
+    idx: int, optional
+        offset, same as in overlay()
+    skiplines:
+        vertical offset, shift overlay down by this many lines
+
+    Example
+    -------
+    u = [ "1__________",
+          "2__________",
+          "3__________",
+          "4__________",
+          "5__________" ]
+    o = [ "A   ",
+          " B  ",
+          "  C ",
+          "   D" ]
+    t = " "
+    skiplines = 1
+    idx = 9
+
+    result = [ "1__________",
+               "2_____A____",
+               "3______B___",
+               "4_______C__",
+               "5________D_" ]
+    """
+
+    out = []
+
+    # copy skiplines from underlay without overlay
+    for i in range(min(skiplines, len(under))):
+        out.append(under[i])
+
+    # copy overlaid lines to output
+    for i in range(skiplines, min(len(under), skiplines + len(over))):
+        out.append(overlay(under[i], over[i - skiplines], transparent, idx))
+
+    # copy remaining underlay lines to output without overlay
+    for i in range(min(len(under), skiplines + len(over)), len(under)):
+        out.append(under[i])
+
+    return out
--- lib/keyhole/screen/splash_screen.py
+++ lib/keyhole/screen/splash_screen.py
@@ -1,62 +1,54 @@
 import urwid
+import time
+import random
 
+from .animate import overlay_multiline
 from .screen import Screen
+from .constellation import Constellation
 
 
 class SplashScreen(Screen):
     logo = [
-        "+====+         o ",
-        "|####|     __ /  ",
-        "|####|    /  X   ",
-        "|####|   (  ' )  ",
-        "|####|  / \__/   ",
-        "|####|\/o   /    ",
+        "+====+888888888o8",
+        "|####|88888__ /88",
+        "|####|8888/  X888",
+        "|####|888(  ' )88",
+        "|####|88/ \__/888",
+        "|####|\/o   /8888",
         "|####|/    +====+",
         "|####/o / /|####|",
         "+===/o / X |####|",
-        "   /    / \|####|",
-        "  /\___/   |####|",
-        " (    /    |####|",
-        "  \__/     |####|",
-        "  / /      |####|",
-        " / /       +====+",
-        " `/              ",
-    ]
-
-    constellation = [
-        "                                        ",
-        "                                        ",
-        "                                        ",
-        "                                        ",
-        "                                        ",
-        "                                        ",
-        "                                        ",
-        "      o                                 ",
-        "            *                           ",
-        "                                        ",
-        "                                        ",
-        "                .                       ",
-        "                                        ",
-        "                                        ",
-        "                    *                   ",
-        "                                        ",
-        "                                        ",
-        "                             o          ",
-        "                    o                   ",
-        "                                        ",
-        "                          .             ",
-        "                                        ",
-        "                                        ",
+        "888/    /8\|####|",
+        "88/\___/888|####|",
+        "8(    /8888|####|",
+        "88\__/88888|####|",
+        "88/ /888888|####|",
+        "8/ /8888888+====+",
+        "8`/88888888888888",
     ]
 
     def __init__(self, loop, conf, display):
+        self.starfield = Constellation().get_rand()
+        self.star_delay = 1
+        self.frame_delay = .06
+        self.elevation = random.randint(0, len(self.starfield) - len(self.logo))
         super().__init__(loop, conf, display)
 
     def set_screen_name(self):
         self.display.set_screen_name("Welcome")
 
-    def set_contents(self):
-        self.display.set_body(self.build_logo(self.constellation))
+    def set_contents(self, loop=None, data=None):
+        if data == None:
+            self.display.set_body(self.build_logo(self.starfield))
+        else:
+            lines = overlay_multiline(
+                self.starfield,
+                self.logo,
+                transparent="8",
+                idx=data,
+                skiplines=self.elevation,
+            )
+            self.display.set_body(self.build_logo(lines))
 
     def set_status(self):
         self.display.set_status("Initializing")
@@ -64,7 +56,7 @@ class SplashScreen(Screen):
     def set_theme(self):
         self.loop.screen.register_palette(self.display.palette["space"])
 
-    def build_logo(self, lines):
+    def build_logo(self, lines, data=None):
         pile = []
         for line in lines:
             pile.append(urwid.Text(line, align="center"))
@@ -77,3 +69,19 @@ class SplashScreen(Screen):
         pile = urwid.Pile(pile)
         pile = urwid.Filler(pile)
         return urwid.AttrMap(pile, "logo")
+
+    def animate(self):
+        frames = len(self.logo[0]) + len(self.starfield[0])
+        start = time.time() + self.star_delay
+        for i in range(frames):
+            alarm = start + (i * self.frame_delay)
+            self.loop.set_alarm_at(alarm, self.set_contents, i)
+
+    def activate(self, loop=None, data=None):
+        self.display.set_status("")
+        self.set_screen_name()
+        self.set_contents()
+        self.set_status()
+        self.set_keys()
+        self.set_theme()
+        self.animate()
